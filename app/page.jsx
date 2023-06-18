2a7b2dacb7a02e57ed420b61765c45c485180c63
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2023-06-18 10:51:17 -0600

    working on loader

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,30 @@
 "use client"
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 // Homepage
 import Homepage from 'layout/Homepage';
 
+// Loader
+import Loader from '../components/Loader';
+
+
+
+
+
 const page = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+
+useEffect(() => {
+  // simulating async action
+  setTimeout(() => {
+    setIsLoading(false)
+  }, 2000)
+}, [])
+
   return (
   <>
-<Homepage/>
+{isLoading ? <Loader/> : <Homepage/>}
   </>
   )
 }
